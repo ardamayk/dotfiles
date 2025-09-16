9885ed01df95d8e9cc57417d6c4b0624027f08fb
ardamayk <ardamike21@hotmail.com>
2025-09-16 03:54:02 +0300

    all are done

--- install.sh
+++ install.sh
@@ -1,33 +1,42 @@
 # pacstrap
-pacstrap -K /mnt base base-devel linux linux-firmware sudo networkmanager iwd grub efibootmgr
+pacstrap -K /mnt base base-devel linux linux-firmware sudo networkmanager iwd grub efibootmgr nvim sudo man
+
+# ----------------------------------------
 
 # post install, after chroot
-sudo pacman -S --needed nvidia-dkms nvidia-utils nvidia-settings linux-headers
 
-# first opening
 # nmcli config
-sudo systemctl enable --now NetworkManager
-sudo pacman -Rns nm-applet
-nmcli config
+# nmcli config
+# [device]
+# wifi.backend
+sudo systemctl enable NetworkManager
+# sudo pacman -Rns nm-applet
 
-# utility
-sudo pacman -S nvim chezmoi zsh tmux
+sudo pacman -S --needed nvidia-dkms nvidia-utils nvidia-settings linux-headers
+sudo pacman -S chezmoi zsh git ssh
 
-# tmux ayarlamalari
+# chezmoi ayarlamalari
+chezmoi init me.github:ardamayk/dotfiles.git
 
 # zshell ayarlamalari
 chsh -s /bin/zsh
 # oh-my-zshell
 
-# Dotfiles install
-chezmoi init me.github:ardamayk/dotfiles.git
+# git ayarlamalari
+git config edit --global
 
-# Utility apps
-sudo pacman -S fuzzel
-sudo pacman -S eza zoxide keyd xdg-open fd fzf nvm tar unzip unrar
+# ----------------------------------------
 
-# keyd config
-sudo systemctl enable --now keyd
+# first opening
+
+# utility
+sudo pacman -S tmux keyd
+
+# tmux ayarlamalari
+
+# keyd ayarlamalari
+# keyd dosyalarini gondermek gerekiyor
+systemctl enable --now keyd
 
 # # yay install
 # sudo pacman -S yay
@@ -42,21 +51,15 @@ sudo pacman -S noto-fonts-emoji
 sudo pacman -S sddm
 
 # Hyprland install
+# hyprland kurulumu icin swaync nin kurulmasi gerekiyor
+# monitor degerinin hyprland config icinde ayarlanmasi gerekiyor
 sudo pacman -S hyprland xdg-desktop-portal-hyprland uwsm
 /usr/share/wayland-sessions
 zshell highlighting
 
+# hyprpaper in calismasi icin monitor adinin dogru verilmesi gerekiyor
 # Hypr applications install
-sudo pacman -S hyprpaper hyprlock hypridle hyprpicker
-
-# Taskbar install
-sudo pacman -S waybar
-
-# sound install
-sudo pacman -S pavucontrol
-
-# bluetooth install
-sudo pacman -S bluetoothctl bluetui
+sudo pacman -S hyprpaper hyprlock hypridle hyprpicker hyprshot
 
 # Notificiaton center install
 sudo pacman -S swaync
@@ -64,16 +67,39 @@ sudo pacman -S swaync
 # Terminal installation
 sudo pacman -S alacritty
 
+# Utility apps
+sudo pacman -S fuzzel
+sudo pacman -S eza zoxide keyd xdg-open fd fzf nvm tar unzip unrar wl-clipboard
+
+# Taskbar install
+sudo pacman -S waybar
+# clock un calismasi icin locale-gen ayarlarinin yapilmasi gerekiyor, eger /etc/locale.conf yapilacaksa /etc/locale.gen ile ayni degere set edilmesi gerekiyor!!!
+# workspace in calismasi icin monitor isminin dogru yazilmasi gerekiyor, duzeltilmesi gerekiyor
+
+# sound install
+sudo pacman -S pipewire wireplumber pipewire-pulse pipewire-alsa pavucontrol
+# pulseaudio veya alsa gerekmiyor ? neden gerekmiyor peki
+# enable islemleri gerekiyor, pulseaudio nun disable --now yapilmasi gerekiyor
+# --user olarak calisiyorlar, server sistemi, session sistemi var wireplumber
+
+#  wifi install
+sudo pacman -S impala
+# oncelikle nmcli ve iwd nin kurulmasi gerekiyor
+
+# bluetooth install
+sudo pacman -S bluez   # bluetoothctl geliyor bununla, ayrica bluetootd geliyor olmali
+sudo pacman -S bluetui # veya bluez-utils
+
+# system management
+sudo pacman -S btop
+
 # xdg-mime config
 
 # GUI apps
 typora obsidian mullvad-vpn mullvad-browser
 
-# system management
-sudo pacman -S btop
-
 # dev-tools
-arp-scan ip-tables wl-copy
+arp-scan ip-tables
 
 # Terminal apps
 qemu nftables
@@ -81,6 +107,3 @@ docker virt-manager electron
 
 # flatpak
 sudo pacman -S flatpak
-
-#  wifi install
-sudo pacman -S impala
