9f63dfbb7ee1df630d097d583c295424acfe2c5a
ardamayk <ardamike21@hotmail.com>
2025-09-22 15:41:43 +0000

    keyd and NetworkManager configs added, install.sh updated

--- install.sh
+++ install.sh
@@ -13,18 +13,18 @@ sudo systemctl enable NetworkManager
 # sudo pacman -Rns nm-applet
 
 sudo pacman -S --needed nvidia-dkms nvidia-utils nvidia-settings linux-headers
-sudo pacman -S chezmoi zsh git ssh
+sudo pacman -S chezmoi zsh git openssh zoxide
 
 # chezmoi ayarlamalari
 chezmoi init me.github:ardamayk/dotfiles.git
 
 # zshell ayarlamalari
-chsh -s /bin/zsh
+# chsh -s /bin/zsh
 sudo pacman -S zsh-syntax-highlighting
 # oh-my-zshell
 
-# git ayarlamalari
-git config edit --global
+# # git ayarlamalari
+# git config edit --global
 
 # ----------------------------------------
 
@@ -42,15 +42,6 @@ systemctl enable --now keyd
 # # yay install
 # sudo pacman -S yay
 
-# font for browser
-fc-cache -fv
-
-# Icon pack install
-sudo pacman -S ttf-nerd-fonts-symbols
-
-# Emoji pack install
-sudo pacman -S noto-fonts-emoji
-
 # Display Manager install
 sudo pacman -S sddm
 
@@ -62,8 +53,8 @@ sudo pacman -S hyprland xdg-desktop-portal-hyprland uwsm
 
 # hyprpaper in calismasi icin monitor adinin dogru verilmesi gerekiyor
 # Hypr applications install
-# hyprpoolkitagent'in GUI araclarinin duzgun calismasi icin gerekli
-sudo pacman -S hyprpaper hyprlock hypridle hyprpicker hyprshot hyprpoolkitagent
+# hyprpolkitagent'in GUI araclarinin duzgun calismasi icin gerekli
+sudo pacman -S hyprpaper hyprlock hypridle hyprpicker hyprshot hyprpolkitagent
 
 # Notificiaton center install
 sudo pacman -S swaync
@@ -73,12 +64,20 @@ sudo pacman -S alacritty
 
 # Utility apps
 sudo pacman -S fuzzel
-sudo pacman -S eza zoxide keyd xdg-open fd fzf nvm tar unzip unrar wl-clipboard
+sudo pacman -S eza xdg-utils fd fzf tar unzip unrar wl-clipboard
 
-# Taskbar install
-sudo pacman -S waybar
-# clock un calismasi icin locale-gen ayarlarinin yapilmasi gerekiyor, eger /etc/locale.conf yapilacaksa /etc/locale.gen ile ayni degere set edilmesi gerekiyor!!!
-# workspace in calismasi icin monitor isminin dogru yazilmasi gerekiyor, duzeltilmesi gerekiyor
+# dev-tools
+sudo pacman -S nvm
+
+# font for browser
+# bu komutu goremiyorum, bu komutu indirmem gerekiyor.
+fc-cache -fv
+
+# Icon pack install
+sudo pacman -S ttf-nerd-fonts-symbols
+
+# Emoji pack install
+sudo pacman -S noto-fonts-emoji
 
 # sound install
 sudo pacman -S pipewire wireplumber pipewire-pulse pipewire-alsa pavucontrol
@@ -87,16 +86,25 @@ sudo pacman -S pipewire wireplumber pipewire-pulse pipewire-alsa pavucontrol
 # --user olarak calisiyorlar, server sistemi, session sistemi var wireplumber
 sudo pacman -S playerctl
 
+# Taskbar install
+sudo pacman -S waybar
+# clock un calismasi icin locale-gen ayarlarinin yapilmasi gerekiyor, eger /etc/locale.conf yapilacaksa /etc/locale.gen ile ayni degere set edilmesi gerekiyor!!!
+# workspace in calismasi icin monitor isminin dogru yazilmasi gerekiyor, duzeltilmesi gerekiyor
+
 #  wifi install
 sudo pacman -S impala
+systemctl enable --now iwd
 # oncelikle nmcli ve iwd nin kurulmasi gerekiyor
 
 # bluetooth install
-sudo pacman -S bluez   # bluetoothctl geliyor bununla, ayrica bluetootd geliyor olmali
+sudo pacman -S bluez # bluetoothctl geliyor bununla, ayrica bluetootd geliyor olmali
+systemctl enable --now bluetooth.service
 sudo pacman -S bluetui # veya bluez-utils
 
 # system management
-sudo pacman -S btop
+Sudo pacman -S btop
+
+reboot
 
 # xdg-mime config
 
